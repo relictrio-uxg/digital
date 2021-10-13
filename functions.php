2c9a51bb97f813dce9f492f0ead6563077e15091
bhavanaS <uxg.bhavana@gmail.com>
2021-10-13 19:15:30 +0530

    added image

--- functions.php
+++ functions.php
@@ -18,6 +18,7 @@
         wp_enqueue_style('variables', get_template_directory_uri()."/assets/css/partials/variables.css", array(), $version, 'all');
         wp_enqueue_style('media', get_template_directory_uri()."/assets/css/media.css", array(), $version, 'all');
         wp_enqueue_style('home-style', get_template_directory_uri()."/assets/css/home.css", array(), $version, 'all');
+        // wp_enqueue_style('chat-style', get_template_directory_uri()."/assets/css/chat.css", array(), $version, 'all');
        
 		
 	}
