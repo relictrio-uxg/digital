604e9837f5cc13c10ab82093562c0189c5f8910c
bhavanaS <uxg.bhavana@gmail.com>
2021-10-19 12:11:00 +0530

    added chatbox js

---
+++ assets/js/chat.js
@@ -0,0 +1,9 @@
+let msgExpansion = document.querySelector("#chat__expand");
+let msg = document.querySelector(".chat__sec__head");
+
+
+function myFunction(){
+    msgExpansion.style.display = "block";
+}
+
+
